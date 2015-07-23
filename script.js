934d7ade056605cf7a1f8aefa21f07016134f41f
Cena Mayo <cenazoic@gmail.com>
2015-07-23 16:00:34 -0500

    working GiantBomb API

--- script.js
+++ script.js
@@ -1,46 +1,66 @@
 var apikey = '3ce9c787e57ca42e61b85ba9c89abf198a78684d'; // Put your API key here
 
-// Use this function to do stuff with your results. 
-// It is called after 'search' is executed.
-function searchCallback(results) {
-    console.log(results);
-
-
-	$('body').append("<div class='gameName'>" + results[0].image[2] + " " + results[0].name + "</div>");
-	$('body').on('click', '#gameName', function() {
-
-		var $el = $('body').children().last();
-
-		$el.append('<p>' + 'Description: ' + results[0].desk + '</p>');
-		$el.append('<p>' + 'Release Date: ' + results[0].original_release_date + '</p>');
-		$el.append('<p>' + 'Platform: ' + results[0].platform + '</p>');
-
-	});
-}
-
 $(document).ready(function() {
-
-
 
 	// Start the search here
 
-	$('#search').submit(function() {
+	$('#search').on('click', function(event) {
+		event.preventDefault();
 
-	
 		var $input = $("#search :input");
 		console.log("This is inputs:" + $input);
 		
 		var values = {};
 		
 		console.log($input.val());
-		search($input.val());
-
+	
+		search ($input.val());
 	});
 
-});
+
+// Use this function to do stuff with your results. 
+// It is called after 'search' is executed.
+function searchCallback(results) {
+    console.log(results);
+	var $el = $('body').children().last();
+
+    for (var i = 0; i < results.length; i++) {
+
+    	if (results) {
+
+		$('body').append("<div class='sortable'><button id='info'>Click for more info!</button>" + results[i].name + "</div>");
+
+		for (var k=0; k < results[i].images.length; k++) {
+
+			$('body').append("<p><img src='results[i].images[k].XXX)'></p>");
+
+		}
+
+		}
+		else  {
+		$('body').append("<div><p> 'No results found!'</p></div>");
+		}
+
+	}
+
+	$(this).on('click', '#info', function() {
+
+		$(this).append('<p>' + 'Description: ' + this.description + '</p>');
+			$(this).append('<p>' + 'Release Date: ' + this.original_release_date + '</p>');
+
+
+				for (var j = 0; j < this.platforms.length; j++) {
+			$(this).append('<p>' + 'Platform: ' + this.platforms[j].name + '</p>');
+		}
+	});
+
+
+}
+
 
 // HELPER FUNCTION
 // Executes a search using 'query' and runs searchCallback on the results of a success.
+
 function search(query){
 
 	$.ajax ({
@@ -54,7 +74,11 @@ function search(query){
 	    },
 	    success: function(data) {
 	        searchCallback(data.results);
-	    }
-	});
+	    },
 
+	    error: function(errorThrown) { 
+                    alert("Status: ERROR"); alert("Error: " + errorThrown);
+	}
+});
 }
+});
